3790112f6a38fd75d1577b0bda95850141785694
ykikoqAq <974032325@qq.com>
2024-03-25 21:02:08 +0800

    test-openwrt

--- preset-openwrt/1.clone.sh
+++ preset-openwrt/1.clone.sh
@@ -28,7 +28,9 @@ git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/sbwml/luci-app-alist.git
 git clone --depth 1 https://github.com/yichya/luci-app-xray.git && mv luci-app-xray/core luci-app-xray-core && mv luci-app-xray/status luci-app-xray-status && rm -rf luci-app-xray
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
-git clone --depth 1 https://github.com/vernesong/OpenClash.git && mv OpenClash/luci-app-openclash ./ && rm -rf OpenClash
+# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
+# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
+# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
+# git clone --depth 1 https://github.com/vernesong/OpenClash.git && mv OpenClash/luci-app-openclash ./ && rm -rf OpenClash
+git clone --depth 1 https://github.com/sirpdboy/luci-app-lucky.git lucky
+git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git ddns-go
